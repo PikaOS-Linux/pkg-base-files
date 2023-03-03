07858f7e2aa967bd63626cfb5551fc0417915284
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-03 17:38:57 +0300

    nvm I'm stupid

--- main.sh
+++ main.sh
@@ -12,8 +12,9 @@ apt-get build-dep ./ -y
 
 # Build package
 LOGNAME=root dh_make --createorig -y -l -p base-files_12.2ubuntu99
-dpkg-buildpackage  --no-sign
+dpkg-buildpackage
 
 # Move the debs to output
 cd ../
 mkdir -p ./output
+mv ./*.deb ./output/
