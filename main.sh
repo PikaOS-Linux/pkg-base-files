d9a0b50f894fcc8658d4210e0490fd8da140bd11
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-16 13:27:59 +0300

    Update to dynamic branding

--- main.sh
+++ main.sh
@@ -1,3 +1,9 @@
+# Get logos
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-mono-small.svg -O ./basefiles/logos/pika-logo-duotone.svg
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo-text-dark.svg -O ./basefiles/logos/pika-logo-text-dark.svg
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo-text.svg -O ./basefiles/logos/pika-logo-text.svg
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo.svg -O ./basefiles/logos/pika-logo.svg
+
 # Clone Upstream
 cd ./basefiles
 
